316a3fb05bec405f17271df5c4b94130051c6b7d
Bruce Hu <brucehu@wireless-10-147-18-84.public.utexas.edu>
2022-10-11 17:43:58 -0500

    jkhkjhkj

---
+++ code2/NovReg.py
@@ -0,0 +1,8 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import re
+from sklearn import linear_model
+import statsmodels.api as sm
+from mpl_toolkits.mplot3d import Axes3D
