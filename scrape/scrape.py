e4bc6a31e1ef4c0f81e24278901474bf182d5052
Qin Xia <qx744@utexas.edu>
2022-10-12 01:09:43 -0500

    scrape.py,#7

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -57,7 +57,7 @@ def crawl_booking_data(year,month):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
     }
     for i in range(10): 
-        
+
         url = 'https://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggI46AdIM1gEaLACiAEBmAExuAEHyAEP2AEB6AEB-AECiAIBqAIDuAKnzYuaBsACAdICJGRlYTQ0OTAxLTJjYjEtNGQ5Yi1iZWIyLTFkYzNhNjY4YWVmZtgCBeACAQ&sid=97ce27f6e33910f88c17fb7c197ba18b&aid=304142&tmpl=searchresults&ac_click_type=b&ac_position=0&checkin_month='+month+'&checkin_monthday=21&checkin_year='+year+'&checkout_month='+month+'&checkout_monthday=27&checkout_year='+year+'&class_interval=1&dest_id=20126394&dest_type=city&dtdisc=0&efdco=1&from_sf=1&group_adults=2&group_children=0&iata=AUS&inac=0&index_postcard=0&label_click=undef&lang=en-us&no_rooms=1&offset='+str(i*25)+'&postcard=0&raw_dest_type=city&room1=A%2CA&sb_price_type=total&search_selected=1&shw_aparth=1&slp_r_match=0&soz=1&src=index&src_elem=sb&srpvid=06a07270f9440067&ss=Austin%2C+Texas%2C+United+States&ss_all=0&ss_raw=austin&ssb=empty&sshis=0&ssne=New+York&ssne_untouched=New+York&lang_click=other&cdl=zh-cn&lang_changed=1'
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, 'lxml')
@@ -67,6 +67,9 @@ def crawl_booking_data(year,month):
             h_price = findprice(table)
             h_score = findscore(table)
             h_dis = finddis(table)
+            if(h_name not in h_names and len(h_score)>0 and len(h_dis)>0):
+                data.append([h_name,h_price,float(h_score),float(h_dis)])
+                h_names.append(h_name)
     return data
 
 
