5fb42e92dc9b513f841be314677305337b1c311e
Qin Xia <qx744@utexas.edu>
2022-10-12 02:13:58 -0500

    code2

---
+++ code2/Nov_price_is_y.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import re
+from sklearn import linear_model
+import statsmodels.api as sm
+from mpl_toolkits.mplot3d import Axes3D
+
+
+df11 = pd.read_csv('/Users/brucehu/Desktop/Nov.csv')
+
+
