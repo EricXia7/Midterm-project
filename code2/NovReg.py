b8ed005550f38d95a430e631e9e3a3c5b7636b09
Bruce Hu <brucehu@Bruces-MacBook-Air.local>
2022-10-12 00:55:10 -0500

    Dec

--- code2/NovReg.py
+++
@@ -1,8 +0,0 @@
-
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import re
-from sklearn import linear_model
-import statsmodels.api as sm
-from mpl_toolkits.mplot3d import Axes3D
