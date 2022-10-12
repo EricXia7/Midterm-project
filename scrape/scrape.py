1541d816d3a32b0ed0d72f60def748d6fbada06b
Jingru Li <112587000+koryli@users.noreply.github.com>
2022-10-12 00:35:12 -0500

    scrape frame

---
+++ scrape/scrape.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+
+def findname(table):
+    '''
+      Scrape hotel name as a string
+    '''
+    return
+
+
+def findprice(table):
+    '''
+      Scrape hotel price as a float
+    '''
+    return
+
+
+def findscore(table):
+    """
+      Scrape hotel rating score as a string
+    """
+    return
+
+
+def finddis(table):
+    '''
+      Scrape the distance of hotels from city centre as a string
+    '''
+
+    return
+
+
+def crawl_booking_data(year,month):
+    '''
+    scrape the information from booking.com then return a list of hotel information 
+    The list element for hotels is['name','price','score','dis']
+    '''
+    return
+
+
+
+
