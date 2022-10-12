4ed18b69f324073a6b77bc97f963adcfa1f29fc2
Jingru Li <112587000+koryli@users.noreply.github.com>
2022-10-12 02:44:46 -0500

    11_plot

---
+++ code2/Nov_Price_y_Plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import re
+from sklearn import linear_model
+import statsmodels.api as sm
+from mpl_toolkits.mplot3d import Axes3D
+
+df11 = pd.read_csv('c:/Users/1Econ/Python/Midterm-project/202211.csv')
+print(df11)
+
