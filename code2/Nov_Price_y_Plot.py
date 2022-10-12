9888cf41721128cd45cb14fe04ab5253f5fc0453
Jingru Li <112587000+koryli@users.noreply.github.com>
2022-10-12 02:48:17 -0500

    11_plot

--- code2/Nov_Price_y_Plot.py
+++ code2/Nov_Price_y_Plot.py
@@ -28,3 +28,38 @@ model = ols.fit(X, Y)
 predicted = model.predict(model_viz)
 
 r2 = model.score(X, Y)
+
+plt.style.use('default')
+
+fig = plt.figure(figsize=(12, 4))
+
+ax1 = fig.add_subplot(131, projection='3d')
+ax2 = fig.add_subplot(132, projection='3d')
+ax3 = fig.add_subplot(133, projection='3d')
+
+axes = [ax1, ax2, ax3]
+
+for ax in axes:
+    ax.plot(x, y, z, color='k', zorder=15, linestyle='none', marker='o', alpha=0.5)
+    ax.scatter(xx_pred.flatten(), yy_pred.flatten(), predicted, facecolor=(0,0,0,0), 
+               s=20, edgecolor='orange')
+    ax.set_xlabel('dis', fontsize=12)
+    ax.set_ylabel('score', fontsize=12)
+    ax.set_zlabel('price', fontsize=12)
+    ax.locator_params(nbins=4, axis='x')
+    ax.locator_params(nbins=5, axis='x')
+    
+ax1.text2D(0.2, 0.32, '', fontsize=13, ha='center', va='center',
+           transform=ax1.transAxes, color='grey', alpha=0.5)
+ax2.text2D(0.3, 1, '', fontsize=13, ha='center', va='center',
+           transform=ax2.transAxes, color='grey', alpha=0.5)
+ax3.text2D(0.85, 0.85, '', fontsize=13, ha='center', va='center',
+           transform=ax3.transAxes, color='grey', alpha=0.5)
+
+ax1.view_init(elev=28, azim=20)
+ax2.view_init(elev=4, azim=114)
+ax3.view_init(elev=60, azim=165)
+
+fig.suptitle('$R^2 = %.2f$' % r2, fontsize=20)
+
+fig.tight_layout()
