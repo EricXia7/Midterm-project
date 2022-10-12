f89fa72c86c1dbadac6702522efba1a080c284dc
Qin Xia <qx744@utexas.edu>
2022-10-12 02:15:01 -0500

    code2

--- code2/Nov_price_is_y.py
+++ code2/Nov_price_is_y.py
@@ -10,3 +10,19 @@ from mpl_toolkits.mplot3d import Axes3D
 df11 = pd.read_csv('/Users/brucehu/Desktop/Nov.csv')
 
 
+plt.style.use('ggplot')
+x = df11[['dis','score']]
+y = df11['price']
+ 
+regr = linear_model.LinearRegression()
+regr.fit(x, y)
+
+
+# In[6]:
+
+
+print('Intercept: \n', regr.intercept_)
+print('Coefficients: \n', regr.coef_)
+
+
+
